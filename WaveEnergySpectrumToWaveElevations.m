12bb6b39d49bdc38122bf588eaad748d49a99f89
GiantChestnutBread <assess09@nate.com>
2018-11-14 02:41:25 +0900

    figure plotting and saving added

--- WaveEnergySpectrumToWaveElevations.m
+++ WaveEnergySpectrumToWaveElevations.m
@@ -2,7 +2,7 @@
 clear ; clc ; close all ;
 
 %% Given for ISSC wave spectrum
-% Conditions to represent the wave spectrum
+% Condition to represent the wave spectrum
 angularFrequencyMax = 4 ;
 
 % Variables to represent to the wave energy spectrum
@@ -16,11 +16,13 @@ angularFrequency = 0:0.01:angularFrequencyMax ;
 waveEnergySpectrum = (A ./ angularFrequency.^5) .* exp(-B ./ angularFrequency.^5) ;
 
 % Visualization of the wave energy spectrum
-figure(1) ;
+waveEnergySpectrumFig = figure ;
+figure(waveEnergySpectrumFig) ;
 plot(angularFrequency, waveEnergySpectrum) ;
 title('ISSC wave spectrum') ;
 xlabel('\omega (rad/sec)') ; ylabel('S_{\eta}(\omega) (m^{2}/(rad/sec))') ;
 grid on ;
+saveas(waveEnergySpectrumFig, 'WaveEnergySpectrumISSC.png') ;
 
 %% Wave elevation time series according to discrete angular frequncy
 % It Needs 
@@ -49,7 +51,8 @@ randomPhaseAngle = rand(1, numBins) * (2*pi) ;
 
 % Wave elevation according to discrete angular frequncdy
 % waveElevation across row: anglularFrequencyIndex, across column: timeStep
-timeStep = 0:0.01:30 ;
+timeMax = 30 ;
+timeStep = 0:0.01:timeMax ;
 for angularFrequencyIndex = 1:length(angularFrequencyDiscrete)
     waveElevation(:, angularFrequencyIndex) =...
         sqrt(2 * waveEnergySpectrumDiscrete(angularFrequencyIndex) * angularFreqeuncyInterval)...
@@ -57,16 +60,44 @@ for angularFrequencyIndex = 1:length(angularFrequencyDiscrete)
         + randomPhaseAngle(angularFrequencyIndex)) ;
 end
 
-% Visualization of the wave elevation accordingto discrete angular wave frequncy
-figure(2) ;
-suptitle('Time singal of wave elevation for all discrete angular frequency')
-NumSubplotColumn = 5 ;
-for angularFrequencyIndex = 1:numBins
 
-    subplot(numBins / NumSubplotColumn, NumSubplotColumn, angularFrequencyIndex) ;
-    plot(timeStep, waveElevation(:, angularFrequencyIndex)) ;
-    angularFrequencyIndexStr = num2str(angularFrequencyIndex) ;
-    title(['\omega_', angularFrequencyIndexStr]) ;
-    grid on ;
+%% Visualization of the wave elevation according to discrete angular wave frequncy
+numSubplotInOneFig = 15 ;
+numFig = ceil(numBins / numSubplotInOneFig) ;
+numSubplotColumn = 3 ;
+numSubplotRow = ceil(numSubplotInOneFig / numSubplotColumn) ;
+angularFrequencyIndex = 0 ;     
+
+for figIndex = 1:numFig
+    waveElevationFig(figIndex) = figure ;
+    figure(waveElevationFig(figIndex)) ;
+    set(waveElevationFig(figIndex), 'position', [0 0 (numSubplotColumn * 300) (numSubplotRow * 150)]) ;
+    tempAngularFrequencyIndexHistory = [] ;     % reset the angular frequency index history
     
+    for subplotCount = 1:numSubplotInOneFig
+        angularFrequencyIndex = angularFrequencyIndex + 1 ;
+        if angularFrequencyIndex > numBins
+            break
+        end
+        tempAngularFrequencyIndexHistory(subplotCount) = angularFrequencyIndex ;    % Save angular frequency index history for subgroup title
+        
+        subplot(numSubplotRow, numSubplotColumn, subplotCount) ;
+        plot(timeStep, waveElevation(:, angularFrequencyIndex)) ;
+        axis([0 timeMax -max(max(waveElevation)) max(max(waveElevation))]) ;
+        xlabel('t (sec)') ; ylabel('\eta(t) (m)') ;
+        title(['\omega_{', num2str(angularFrequencyIndex), '}']) ;
+        grid on ;
+    end
+
+    sgtitle(['Time singal of wave elevation for \omega_{',...
+        num2str(min(tempAngularFrequencyIndexHistory)), '} _- _{',...
+        num2str(max(tempAngularFrequencyIndexHistory)), '}']) ;
+    
+    figSaveName = sprintf('waveElevation%d_%d.png',...
+        min(tempAngularFrequencyIndexHistory),...
+        max(tempAngularFrequencyIndexHistory)) ;
+    
+    saveas(waveElevationFig(figIndex), figSaveName) ;
+
 end
+
